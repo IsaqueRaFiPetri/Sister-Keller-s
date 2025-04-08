4850512e83b08b853e7c797c6075b470ea7ad1c5
IsaqueRaFiPetri <isq.rafipe@gmail.com>
2025-04-08 13:25:45 -0300

    Import ModularFirstPlayerController

--- Sister Keller's Programming Journey/Assets/Scripts/Interactions/InteractableObject.cs
+++ Sister Keller's Programming Journey/Assets/Scripts/Interactions/InteractableObject.cs
@@ -3,15 +3,6 @@ using UnityEngine;
 [RequireComponent(typeof(BoxCollider))]
 public abstract class InteractableObject : MonoBehaviour
 {
-    protected abstract void Interact(); //Obriga o filho a implementar
+    protected abstract void Interact();
 
-    protected void Teste() //O filho tem acesso
-    {
-
-    }
-
-    protected virtual void Teste1() //Permite o uso no filho, mas permite que seja sobrescrito
-    {
-        print("Executando");
-    }
 }
