270e51cc0253854e6bcdd4e25f635bbbe027fc9c
SolDeBotas <mosquitoestudante34234@gmail.com>
2025-05-05 15:31:51 -0300

    Update nos códigos do projeto

---
+++ Sister Keller's Programming Journey/Assets/CodesThatMayBeUseful/MemorieObj.cs
@@ -0,0 +1,11 @@
+public class MemorieObj : InterectableObj
+{
+    protected override void Interact()
+    {
+        MemoriesCounter.Instance.memoriesCount++;
+        PlayerInteract.Instance.OnInteractionEffected.Invoke();
+        Destroy(gameObject);
+        Destroy(transform.parent.gameObject);
+        MemoriesCounter.Instance.Artifact--;
+    }
+}
